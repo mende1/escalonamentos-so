d7e6ac2b934b412da4beba38762b600c5751ccd0
Joao Pedro Fernandes <joao.pedro.fernandes.122@gmail.com>
2023-06-25 18:15:33 -0300

    add missing inputs

--- src/index.js
+++ src/index.js
@@ -3,40 +3,57 @@ let processes = new Array()
 
 function createProcessesDataCollection() {
   ClearTable()
-  let qtd = document.querySelector('#qtd_process').value
+  let qtt = document.querySelector('#qtt_process').value
 
-  if(qtd > 7 || qtd < 1){
+  if(qtt > 7 || qtt < 1){
     return alert('Quantidade de processos deve ser entre 1 e 7')
   }
 
   let processes = document.querySelector('#processes')
   processes.innerHTML = ''
 
-  for(i = 1; i <= qtd; i++){
+  processes.innerHTML = `
+    <label for="quantum">Quantum: </label>
+    <input type="number" name="Quantum do Sistema" id="Quantum" class="input">
+    <label for="overcharge">Sobrecarga do Sistema: </label>
+    <input type="number" name="overcharge" id="overcharge" class="input">
+  `
+
+  for(i = 1; i <= qtt; i++){
     let process = document.createElement('article')
     process.classList.add('process')
     process.innerHTML = `
       <p>Processo <span id="name">${String.fromCharCode(64 + i)}</span></p>
         <div class="process-data">
-        <label for="tempoChegada">Tempo de chegada: </label>
-        <input type="number" name="tempoChegada" id="tempoChegada" class="input">
-        <label for="tempoExecucao">Tempo de execução: </label>
-        <input type="number" name="tempoExecucao" id="tempoExecucao" class="input">
+          <label for="arrivalTime">Tempo de chegada: </label>
+          <input type="number" name="arrivalTime" id="arrivalTime" class="input">
+          <label for="executionTime">Tempo de execução: </label>
+          <input type="number" name="executionTime" id="executionTime" class="input">
+          <label for="priority">Prioridade: </label>
+          <input type="number" name="Priority" id="Priority" class="input">
           <label for="deadline">Deadline: </label>
           <input type="number" name="deadline" id="deadline" class="input">
         </div>`
 
     processes.appendChild(process)
   }
+
+  let exe = document.querySelector('.execution')
+  exe.innerHTML = `<button class="btn" onclick="FIFO()">FIFO</button>
+  <button class="btn" onclick="SJF()">SJF</button>
+  <button class="btn">Round Robin</button>
+  <button class="btn">EDF</button>`
+  let total = document.querySelector(`#turnaround`)
+  total.innerHTML = ''
 }
 
 function CreateProcesses() {
   let processesData = document.querySelectorAll('.process')
   processesData.forEach(process => {
     let newProcess = new Object()
-    newProcess.nome = process.querySelector('#name').innerHTML
-    newProcess.tempoExecucao = process.querySelector('#tempoExecucao').value
-    newProcess.tempoChegada = process.querySelector('#tempoChegada').value
+    newProcess.name = process.querySelector('#name').innerHTML
+    newProcess.executionTime = process.querySelector('#executionTime').value
+    newProcess.arrivalTime = process.querySelector('#arrivalTime').value
     newProcess.deadline = process.querySelector('#deadline').value
     processes.push(newProcess)
   });
@@ -51,76 +68,76 @@ function CreateTable() {
 
   processes.forEach(process => {
     table.innerHTML += `
-      <tr id="processo" class="${process.nome}">
-      <td id="${process.nome}">${process.nome}</td>
+      <tr id="process" class="${process.name}">
+      <td id="${process.name}">${process.name}</td>
       </tr>`
   });
   
-  let processos = document.querySelectorAll('#processo')
+  let tableProcessos = document.querySelectorAll('#process')
   
-  processos.forEach(processo => {
+  tableProcessos.forEach(process => {
     for(let i = 1; i <= 40; i++){
-      processo.innerHTML += `
+      process.innerHTML += `
         <td id="tempo${i}"></td>
       `
     }
   });
 }
 
-function EscalonamentoFIFO() {
+function FIFO() {
   CreateProcesses()
   CreateTable()
   processes.sort((a, b) => {
-    return a.tempoChegada - b.tempoChegada
+    return a.arrivalTime - b.arrivalTime
   })
 
   ExecuteFIFOAndSJF(processes)
 }
 
-function EscalonamentoSJF() {
+function SJF() {
   CreateProcesses()
   CreateTable()
   
   processes.sort((a, b) => {
-    return a.tempoChegada - b.tempoChegada
+    return a.arrivalTime - b.arrivalTime
   })
 
-  let fila = new Array()
+  let order = new Array()
 
   for(let tempo = 0; tempo <= 40; tempo++){
-    let filter = processes.filter(process => process.tempoChegada <= tempo)
+    let filter = processes.filter(process => process.arrivalTime <= tempo)
     if(filter.length > 0){
       filter.sort((a, b) => {
-        return a.tempoExecucao - b.tempoExecucao
+        return a.executionTime - b.executionTime
       })
       filter.forEach(process => {
-        fila.push(process)
-        tempo += parseInt(process.tempoExecucao)
+        order.push(process)
+        tempo += parseInt(process.executionTime)
         processes.splice(processes.indexOf(process), 1)
       })
     }
   }  
-  ExecuteFIFOAndSJF(fila) 
+  ExecuteFIFOAndSJF(order) 
 }
 
 function ExecuteFIFOAndSJF(processes) {
-  let tempo = 0
+  let time = 0
   let turnAround = 0
   
   processes.forEach(process => {
-    let = tableProcess = document.querySelector(`.${process.nome}`)
+    let = tableProcess = document.querySelector(`.${process.name}`)
     
-    if(tempo < process.tempoChegada){
-      tempo = parseInt(process.tempoChegada)
+    if(time < process.arrivalTime){
+      time = parseInt(process.arrivalTime)
     }
     
-    for(let i = 1; i <= process.tempoExecucao; i++){
-      let cel = tableProcess.querySelector(`#tempo${i + tempo}`)
-      cel.classList.add('executando') 
+    for(let i = 1; i <= process.executionTime; i++){
+      let cel = tableProcess.querySelector(`#tempo${i + time}`)
+      cel.classList.add('executing') 
     }
     
-    tempo += parseInt(process.tempoExecucao)
-    turnAround += (tempo - parseInt(process.tempoChegada))
+    time += parseInt(process.executionTime)
+    turnAround += (time - parseInt(process.arrivalTime))
   });
 
   turnAround = turnAround / processes.length
@@ -137,8 +154,8 @@ function ClearTable() {
   table.innerHTML = ''
   processes = new Array()
   let exe = document.querySelector('.execution')
-  exe.innerHTML = `<button class="btn" onclick="EscalonamentoFIFO()">FIFO</button>
-  <button class="btn" onclick="EscalonamentoSJF()">SJF</button>
+  exe.innerHTML = `<button class="btn" onclick="FIFO()">FIFO</button>
+  <button class="btn" onclick="SJF()">SJF</button>
   <button class="btn">Round Robin</button>
   <button class="btn">EDF</button>`
   let total = document.querySelector(`#turnaround`)
