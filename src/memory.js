f9433fde75c8e960bbbc33a5773bf6e1e2913a7b
Joao Pedro Fernandes <joao.pedro.fernandes.122@gmail.com>
2023-07-04 19:35:23 -0300

    fix bug

--- src/memory.js
+++ src/memory.js
@@ -6,7 +6,6 @@ function createMemoryDataCollection() {
   for (let i = 1; i <= qtt; i++) {
     let processArticle = document.createElement('article')
     processArticle.id = 'memory_process'
-    processArticle.classList = 'process'
     processArticle.innerHTML = `
     <h2 class="h2-title">Processo ${i}</h2>
     <div class="memory-data">
@@ -17,10 +16,8 @@ function createMemoryDataCollection() {
     pages.appendChild(processArticle)
   };
   createRealMemory()
-}
-
-function createVirtualMemory() {
-  
+  createVirtualMemory()
+  pageFIFO()
 }
 
 function createRealMemory() {
@@ -40,4 +37,42 @@ function createRealMemory() {
     head.appendChild(hrow)
     body.appendChild(brow)
   }
+}
+
+function createVirtualMemory() {
+  let disc = document.querySelector('#disc')
+  let head = disc.querySelector(`thead`)
+  let body = disc.querySelector(`tbody`)
+  head.innerHTML = ''
+  body.innerHTML = ''
+  let pages = [1,2,3,4,5,5,7,8]
+  let filter = pages.filter((value, index, self) => self.indexOf(value) === index)
+  
+  filter.forEach(page => {
+    let hrow = document.createElement('tr')
+    let brow = document.createElement('tr')
+
+    hrow.innerHTML = `<th>${page}</th>`
+    brow.innerHTML = `<td id="disc-${page}"></td>`
+
+    head.appendChild(hrow)
+    body.appendChild(brow)
+  })
+}
+
+function pageFIFO() {
+  let pages = [1,2,3,4,5,5,7,8]
+  pages.forEach(page => {
+    for(let i = 0 ; i < 50 ; i++) {
+      let ram = document.querySelector(`#ram-${i}`)
+      let disc = document.querySelector(`#disc-${page}`)
+      if(ram.innerHTML == '') {
+        ram.innerHTML = `<span class="atual-page">${page}<span>`
+        break
+      }
+      else if(ram.innerHTML == `<span class="atual-page">${page}<span>`) {
+        
+      }
+    }
+  });
 }
